6fad840e42a1a46c51c338ddd48f7ba72d9a8c4d
yuumni <minyunj77@gmail.com>
2023-11-02 10:51:23 +0900

    새로운 파일 생성 및 ESLint 설정

--- src/pages/PlanningPage.js
+++ src/pages/PlanningPage.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "../styles/PlanningPage.css";
-import Header from "../components/Header";
 import Calendar from "../components/Calendar";
 
 const PlanningPage = () => {
   return (
     <div className="PlanningPage">
-      {/* <Header /> */}
       <Calendar />
     </div>
   );
