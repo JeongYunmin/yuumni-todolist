7e626536b472666134eadae6ca61aac568c047f8
yuumni <minyunj77@gmail.com>
2024-02-04 21:46:06 +0900

    날짜 이동시 할 일 체크 풀리는 이슈 해결

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,31 +2,26 @@ import React, { useState } from "react";
 import "../styles/TodoItem.css";
 
 const TodoItem = ({ el, onClickChecked }) => {
-  const [checked, setChecked] = useState(false);
   const [deleteLine, setDeleteLine] = useState("");
 
-  const handleChange = (e) => {
-    if (!checked) {
-      !e.target.checked;
-      setChecked(true);
+  const handleChange = () => {
+    if (el.isDone) {
       onClickChecked(el.id);
-      setDeleteLine("delete");
-    } else {
-      !e.target.checked;
-      setChecked(false);
+    }
+
+    if (!el.isDone) {
       onClickChecked(el.id);
-      setDeleteLine("");
     }
   };
 
   return (
     <div className="TodoItem">
-      <li className={deleteLine}>
+      <li className={el.isDone ? "delete" : ""}>
         ◆ {el.content}
         <input
           id={el.id}
           type="checkbox"
-          checked={checked}
+          checked={el.isDone}
           onChange={handleChange}
         />
       </li>
