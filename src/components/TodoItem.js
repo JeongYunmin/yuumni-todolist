c28be3796a9b884fcd624e957742f960bba16c6e
yuumni <minyunj77@gmail.com>
2024-02-06 18:39:48 +0900

    투두아이템, localStorage에 저장하는 기능 구현 중

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,7 +1,13 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "../styles/TodoItem.css";
 
 const TodoItem = ({ el, onClickChecked }) => {
+  let localStrData = JSON.stringify(el);
+  let localStrDataId = String(el.id);
+
+  localStorage.setItem(localStrDataId, localStrData);
+  console.log(JSON.parse(localStorage.getItem(localStrDataId)).content);
+
   const handleChange = () => {
     if (el.isDone) {
       onClickChecked(el.id);
@@ -12,9 +18,14 @@ const TodoItem = ({ el, onClickChecked }) => {
     }
   };
 
+  // 새로고침을 하면.. 요소 자체가 사라지는 것 같움ㅠㅠ DOM을 건드려얄 듯 하다..!!!
+
   return (
     <div className="TodoItem">
-      <li className={el.isDone ? "delete" : ""}>
+      <li
+        className={el.isDone ? "delete" : ""}
+        value={JSON.parse(localStorage.getItem(localStrDataId)).content}
+      >
         ◆ {el.content}
         <input
           id={el.id}
