dcbe1bd6ba8aa9ae39be643aff3d179378742912
yuumni <minyunj77@gmail.com>
2024-02-15 15:25:09 +0900

    투두 아이템 삭제기능 구현

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,18 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/TodoItem.css";
+import Btn from "./Btn";
+import Modal from "react-modal";
 
-const TodoItem = ({ todo, onClickChecked }) => {
+const TodoItem = ({ todo, todos, setTodos, onClickChecked }) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
   const handleChange = () => {
     onClickChecked(todo.id);
   };
 
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+  const handleRemove = () => {
+    const updatedTodos = todos.filter((item) => item.id !== todo.id);
+    setTodos(updatedTodos);
+
+    const keys = Object.keys(localStorage);
+    if (keys.includes(String(todo.id))) {
+      localStorage.removeItem(String(todo.id));
+    }
+
+    closeModal();
+  };
+
   return (
-    <div className="TodoItem">
-      <li className={todo.isDone ? "delete" : ""}>
-        ◆ {todo.content}
-        <input type="checkbox" checked={todo.isDone} onChange={handleChange} />
-      </li>
-    </div>
+    <>
+      <div className="TodoItem">
+        <Modal
+          className="modal"
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+        >
+          해당 할 일을 삭제하시겠습니까?
+          <div className="modal-btn-wrapper">
+            <buton className="modal-yes-btn" onClick={handleRemove}>
+              예
+            </buton>
+            <buton className="modal-no-btn" onClick={closeModal}>
+              아니오
+            </buton>
+          </div>
+        </Modal>
+        <li className={todo.isDone ? "delete" : ""}>
+          ◆ {todo.content}
+          <input
+            type="checkbox"
+            checked={todo.isDone}
+            onChange={handleChange}
+          />
+          <Btn onClick={openModal} />
+        </li>
+      </div>
+    </>
   );
 };
 
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -13,12 +13,23 @@ const getFormattedDate = (date) => {
 
 const TodoList = ({ selectedDate }) => {
   const [todos, setTodos] = useState(() => {
-    const storedTodos = localStorage.getItem("todos");
-    return storedTodos ? JSON.parse(storedTodos) : [];
+    const storedTodos = [];
+    const keys = Object.keys(localStorage);
+    keys.forEach((key) => {
+      const item = localStorage.getItem(key);
+      if (item) {
+        storedTodos.push(JSON.parse(item));
+      }
+    });
+    return storedTodos;
   });
 
   useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
+    if (todos) {
+      todos.forEach((todo) => {
+        localStorage.setItem(String(todo.id), JSON.stringify(todo));
+      });
+    }
   }, [todos]);
 
   const handleCreateTodo = (content) => {
@@ -29,7 +40,7 @@ const TodoList = ({ selectedDate }) => {
         isDone: false,
         createdAt: getFormattedDate(new Date(selectedDate)),
       };
-      setTodos((prevTodos) => [...prevTodos, newTodo]);
+      setTodos((prev) => [...prev, newTodo]);
     }
   };
 
@@ -41,25 +52,30 @@ const TodoList = ({ selectedDate }) => {
     );
   };
 
-  const filteredTodos = todos.filter((todo) => {
-    const selectedDateFormatted = getFormattedDate(new Date(selectedDate));
-    return (
-      todo.createdAt.year === selectedDateFormatted.year &&
-      todo.createdAt.month === selectedDateFormatted.month &&
-      todo.createdAt.date === selectedDateFormatted.date
-    );
-  });
+  const filteredTodos =
+    todos &&
+    todos.filter((todo) => {
+      const selectedDateFormatted = getFormattedDate(new Date(selectedDate));
+      return (
+        todo.createdAt.year === selectedDateFormatted.year &&
+        todo.createdAt.month === selectedDateFormatted.month &&
+        todo.createdAt.date === selectedDateFormatted.date
+      );
+    });
 
   return (
     <div className="TodoList">
       <CreateInput handleCreateTodo={handleCreateTodo} />
-      {filteredTodos.map((todo) => (
-        <TodoItem
-          key={todo.id}
-          todo={todo}
-          onClickChecked={handleClickChecked}
-        />
-      ))}
+      {filteredTodos &&
+        filteredTodos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            todos={todos}
+            setTodos={setTodos}
+            onClickChecked={handleClickChecked}
+          />
+        ))}
     </div>
   );
 };
